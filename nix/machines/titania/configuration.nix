ef4ca7980a487081b5de26cdea00711dc0b298cf
Michael Strobel <mstrobel97@gmail.com>
2024-07-24 23:49:39 +0200

    fix: debugging

--- nix/machines/titania/configuration.nix
+++ nix/machines/titania/configuration.nix
@@ -1,10 +1,18 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-    grim
-    slurp
-    chromium
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      wl-clipboard
+      grim
+      slurp
+      chromium
+    ];
+
+    loginShellInit = ''
+      if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+        exec Hyprland
+      fi
+    '';
+  };
 
   programs = {
     hyprland.enable = true;
