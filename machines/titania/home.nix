fd1f0183cf6aed32db123e3e3a3d6b130ead3313
Michael Strobel <mstrobel97@gmail.com>
2024-07-27 00:28:13 +0200

    feat: add battery and backlight

--- machines/titania/home.nix
+++ machines/titania/home.nix
@@ -15,4 +15,26 @@
       "eDP-1, 1920x1080@60, 0x0, 1"
     ];
   };
+
+  programs.waybar.settings.top = {
+    modules-right = [
+      "battery"
+      "backlight"
+    ];
+
+    battery = {
+      format = "{capacity}% <span font='11'>{icon}</span> ";
+      format-charging = "{capacity}% <span font='11'></span>";
+      format-plugged = "{capacity}% <span font='11'></span>";
+      format-icons = [ "" "" "" "" "" ];
+      states = {
+        critical = 25;
+      };
+    };
+
+    backlight = {
+      device = "intel_backlight";
+      format = "{percent}% <span font='11'></span> ";
+    };
+  };
 }
