23ec13f07407e3a8070664ff6c55440a44c8f6a9
Michael Strobel <mstrobel97@gmail.com>
2024-07-28 11:09:35 +0200

    feat: add browser env variable

--- machines/work/configuration.nix
+++ machines/work/configuration.nix
@@ -3,12 +3,17 @@
     ../../modules/i3/default.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    linphone
-    firefox
-    thunderbird
-    rocketchat-desktop
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      linphone
+      firefox
+      thunderbird
+      rocketchat-desktop
+    ];
+    variables = {
+      BROWSER = "firefox";
+    };
+  };
 
   networking.hostName = lib.mkForce "michael-5824";
 }
