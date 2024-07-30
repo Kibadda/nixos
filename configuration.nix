d29b42b543286abfdea2b714fb5f65480dc1712d
Michael Strobel <mstrobel97@gmail.com>
2024-07-30 13:04:34 +0200

    feat: add limit for available boot configs

--- configuration.nix
+++ configuration.nix
@@ -40,7 +40,10 @@ in {
   };
 
   boot.loader = {
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
     efi.canTouchEfiVariables = true;
   };
 
