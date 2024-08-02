af6c60739e6c4de79cae7b5d9751b47c7d9420d8
Michael Strobel <mstrobel97@gmail.com>
2024-08-02 15:03:29 +0200

    feat(work): add overwrite config for personal projects

--- machines/work/home.nix
+++ machines/work/home.nix
@@ -1,7 +1,16 @@
-{ lib, ... }: {
+{ lib, meta, ... }: {
   imports = [
     ../../modules/i3/home.nix
   ];
 
-  programs.git.userEmail = lib.mkForce "m.strobel@cortex-media.de";
+  programs.git = {
+    userEmail = lib.mkForce "m.strobel@cortex-media.de";
+    includes = [
+      {
+        condition = "gitdir:~/Projects/Personal/";
+        contents.user.email = meta.email;
+        contentSuffix = "personal-config";
+      }
+    ];
+  };
 }
