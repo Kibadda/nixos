0958bcc17fd96d787a07f37f7d8b4958aa507f92
Michael Strobel <mstrobel97@gmail.com>
2024-08-02 15:03:54 +0200

    feat(work): change NEOVIM_DIR variable

--- machines/work/home.nix
+++ machines/work/home.nix
@@ -3,6 +3,8 @@
     ../../modules/i3/home.nix
   ];
 
+  home.sessionVariables.NEOVIM_DIR = lib.mkForce "$HOME/Projects/Personal/neovim";
+
   programs.git = {
     userEmail = lib.mkForce "m.strobel@cortex-media.de";
     includes = [
