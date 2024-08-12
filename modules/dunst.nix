b462b105b79a23f808569a22bb494a74c48435f8
Michael Strobel <mstrobel97@gmail.com>
2024-08-12 14:07:43 +0200

    feat(dunst): only enable if hypr or i3 is enabled

--- modules/dunst.nix
+++ modules/dunst.nix
@@ -1,37 +1,41 @@
-{
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        notification_limit = 5;
-        frame_width = 1;
-        frame_color = "#7F3FBF";
-        separator_color = "auto";
-        idle_threshhold = 0;
-        font = "JetBrains Mono Nerd Font 11";
-        show_indicators = "no";
-        sticky_history = false;
-        history_length = 0;
-        corner_radius = 7;
-      };
+{ config, lib, ... }: with lib; let
+  cfg = config.kibadda;
+in {
+  config = mkIf (cfg.hypr.enable || cfg.i3.enable) {
+    services.dunst = {
+      enable = true;
+      settings = {
+        global = {
+          notification_limit = 5;
+          frame_width = 1;
+          frame_color = "#7F3FBF";
+          separator_color = "auto";
+          idle_threshhold = 0;
+          font = "JetBrains Mono Nerd Font 11";
+          show_indicators = "no";
+          sticky_history = false;
+          history_length = 0;
+          corner_radius = 7;
+        };
 
-      urgency_low = {
-        background = "#28143C";
-        foreground = "#FFFFFF";
-        timeout = 5;
-      };
+        urgency_low = {
+          background = "#28143C";
+          foreground = "#FFFFFF";
+          timeout = 5;
+        };
 
-      urgency_normal = {
-        background = "#28143C";
-        foreground = "#FFFFFF";
-        timeout = 5;
-      };
+        urgency_normal = {
+          background = "#28143C";
+          foreground = "#FFFFFF";
+          timeout = 5;
+        };
 
-      urgency_critical = {
-        background = "#28143C";
-        foreground = "#FFFFFF";
-        frame_color = "#FF7F7F";
-        timeout = 120;
+        urgency_critical = {
+          background = "#28143C";
+          foreground = "#FFFFFF";
+          frame_color = "#FF7F7F";
+          timeout = 120;
+        };
       };
     };
   };
