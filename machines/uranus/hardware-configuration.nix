93f4fece4c7f80f2f10391ebfe2daf1a00c0bc27
Michael Strobel <mstrobel97@gmail.com>
2024-08-15 14:45:33 +0200

    feat(uranus): add auto mount for external hdd?

--- machines/uranus/hardware-configuration.nix
+++ machines/uranus/hardware-configuration.nix
@@ -14,6 +14,11 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  # fileSystems."/mnt/external" = {
+  #   device = "/dev/disk/by-uuid/CA3AA5223AA50C8F";
+  #   fsType = "ntfs3";
+  # };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
