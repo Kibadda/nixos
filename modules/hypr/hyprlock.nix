5fc71d9597d7306b786095935f0d3d6b26b2d077
Michael Strobel <mstrobel97@gmail.com>
2024-08-19 15:15:24 +0200

    feat(hypr): move hyprlock to separate module

---
+++ modules/hypr/hyprlock.nix
@@ -0,0 +1,62 @@
+{ pkgs, config, meta, lib, ... }: let
+  cfg = config.kibadda;
+in {
+  config = lib.mkIf cfg.hypr.enable {
+    programs.hyprlock = {
+      enable = true;
+      settings = {
+        background = {
+          monitor = "";
+          path = "/home/${meta.username}/.config/hypr/wallpaper.png";
+          blur_passes = 3;
+          contrast = 0.8916;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0.0;
+        };
+
+        general.grace = 5;
+
+        input-field = {
+          monitor = "";
+          size = "280, 80";
+          outline_thickness = 2;
+          dots_size = 0.2;
+          dots_spacing = 0.2;
+          dots_center = true;
+          outer_color = "rgba(0, 0, 0, 0)";
+          inner_color = "rgba(0, 0, 0, 0.5)";
+          font_color = "rgb(200, 200, 200)";
+          fade_on_empty = false;
+          placeholder_text = "<i><span foreground='#CDD6F4'>Password...</span></i>";
+          hide_input = false;
+          position = "0, -120";
+          halign = "center";
+          valign = "center";
+        };
+
+        label = [
+          {
+            monitor = "";
+            text = "cmd[update:1000] echo \"$(date +\"%-H:%M\")\"";
+            font_size = 120;
+            font_family = "${cfg.font} Nerd Font";
+            position = "0, -300";
+            halign = "center";
+            valign = "top";
+          }
+
+          {
+            monitor = "";
+            text = "Hi there, ${meta.name}";
+            font_size = 25;
+            font_family = "${cfg.font} Nerd Font";
+            position = "0, -40";
+            halign = "center";
+            valign = "center";
+          }
+        ];
+      };
+    };
+  };
+}
