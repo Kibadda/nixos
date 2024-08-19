efa712e011dd8f5570559d4a386a1122417a356b
Michael Strobel <mstrobel97@gmail.com>
2024-08-19 21:36:46 +0200

    feat(uranus): mount hdd on boot

--- machines/uranus/hardware-configuration.nix
+++ machines/uranus/hardware-configuration.nix
@@ -14,10 +14,10 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  # fileSystems."/mnt/external" = {
-  #   device = "/dev/disk/by-uuid/CA3AA5223AA50C8F";
-  #   fsType = "ntfs3";
-  # };
+  fileSystems."/mnt/external" = {
+    device = "/dev/disk/by-uuid/CA3AA5223AA50C8F";
+    fsType = "ntfs3";
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
