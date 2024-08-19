1ce3249e34ce35e7019df12e6f9996ca8556f7bb
Michael Strobel <mstrobel97@gmail.com>
2024-08-19 15:14:51 +0200

    feat(hypr): move hypridle to separate module

---
+++ modules/hypr/hypridle.nix
@@ -0,0 +1,38 @@
+{ pkgs, config, lib, ... }: let
+  cfg = config.kibadda;
+in {
+  config = lib.mkIf cfg.hypr.enable {
+    wayland.windowManager.hyprland.settings.exec-once = [
+      "${pkgs.hypridle}/bin/hypridle"
+    ];
+
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          before_sleep_cmd = "loginctl lock-session";
+        };
+
+        listener = [
+          {
+            timeout = 300;
+            on-timeout = "loginctl lock-session";
+          }
+
+          {
+            timeout = 330;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
+    };
+  };
+}
