558be7ab517a067c189d724fc4645385ef90eaf3
Michael Strobel <mstrobel97@gmail.com>
2024-08-27 23:51:29 +0200

    feat(kitty): add pass_keys.py

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -94,9 +94,52 @@ in {
       };
     };
 
-    home.file.".config/kitty/tool.conf".text = ''
-      include ./kitty.conf
-      clear_all_shortcuts yes
-    '';
+    home.file = {
+      ".config/kitty/tool.conf".text = ''
+        include ./kitty.conf
+        clear_all_shortcuts yes
+      '';
+
+      ".config/kitty/pass_keys.py".text = ''
+        import re
+
+        from kittens.tui.handler import result_handler
+        from kitty.key_encoding import KeyEvent, parse_shortcut
+
+        def encode_key_mapping(window, key_mapping):
+          mods, key = parse_shortcut(key_mapping)
+          event = KeyEvent(
+            mods=mods,
+            key=key,
+            shift=bool(mods & 1),
+            alt=bool(mods & 2),
+            ctrl=bool(mods & 4),
+            super=bool(mods & 8),
+            hyper=bool(mods & 16),
+            meta=bool(mods & 32),
+          ).as_window_system_event()
+
+          return window.encoded_key(event)
+
+        def main():
+          pass
+
+        @result_handler(no_ui=True)
+        def handle_result(args, result, target_window_id, boss):
+          direction = args[1]
+          key_mapping = args[2]
+
+          window = boss.window_id_map.get(target_window_id)
+
+          if window is None:
+            return
+
+          if any(re.search("nvim", p['cmdline'][0] if len(p['cmdline']) else ''', re.I) for p in window.child.foreground_processes):
+            for keymap in key_mapping.split(">"):
+              window.write_to_child(encode_key_mapping(window, keymap))
+          else:
+            boss.active_tab.neighboring_window(direction)
+      '';
+    };
   };
 }
