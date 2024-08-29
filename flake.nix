c70e6e3d90e1e6472d25b71cf7501561a7704b4d
Michael Strobel <mstrobel97@gmail.com>
2024-08-29 06:25:22 +0200

    chore: add additional substituters

--- flake.nix
+++ flake.nix
@@ -26,9 +26,11 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://raspberry-pi-nix.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
     ];
   };
 
