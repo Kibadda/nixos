ca36ba6ab87461af698fe976ab0fa1e25009cd9c
Michael Strobel <mstrobel97@gmail.com>
2024-08-29 22:20:46 +0200

    fix: pi image

--- machines/pi/configuration.nix
+++ machines/pi/configuration.nix
@@ -1,8 +1,13 @@
-{
+{ lib, ... }: {
   raspberry-pi-nix.board = "bcm2711";
 
   services = {
     openssh.enable = true;
     k3s.enable = true;
+  };
+
+  networking = {
+    networkmanager.enable = lib.mkForce false;
+    interfaces.wlan0.useDHCP = true;
   };
 }
