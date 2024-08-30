fc73904b6ddd92637a1d86766cbeb6cac5a24138
Michael Strobel <mstrobel97@gmail.com>
2024-08-30 23:09:41 +0200

    feat(pi): update configuration

--- machines/pi/configuration.nix
+++ machines/pi/configuration.nix
@@ -1,7 +1,16 @@
 { meta, ... }: {
   raspberry-pi-nix.board = "bcm2711";
 
-  services.k3s.enable = true;
+  services.k3s = {
+    enable = true;
+    clusterInit = true;
+    extraFlags = toString [
+      "--write-kubeconfig-mode \"0644\""
+      "--disable servicelb"
+      "--disable traefik"
+      "--disable local-storage"
+    ];
+  };
 
   networking = {
     wireless = {
@@ -9,6 +18,10 @@
       networks.${meta.wifi.ssid}.psk = meta.wifi.pass;
     };
     interfaces.wlan0.useDHCP = true;
-    firewall.allowedTCPPorts = [ 80 443 ];
+    firewall.allowedTCPPorts = [ 80 443 6443 ];
   };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  boot.kernelParams = [ "cgroup_memory=1" "cgroup_enable=memory" ];
 }
