cadf56c1236df486406b9b1da1430b7a35c55bc7
Michael Strobel <mstrobel97@gmail.com>
2024-09-09 23:57:40 +0200

    feat(pi): add first helmfile with immich

--- machines/pi/home.nix
+++ machines/pi/home.nix
@@ -3,4 +3,40 @@
     kitty.enable = false;
     yubikey.enable = false;
   };
+
+  home = {
+    sessionVariables = {
+      KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+    };
+
+    file = {
+      ".config/k3s/helmfile.yaml".text = ''
+        repositories:
+          - name: immich
+            url: https://immich-app.github.io/immich-charts
+        ---
+        releases:
+          - name: immich
+            namespace: immich
+            chart: immich/immich
+            version: 0.7.2
+            values:
+              - ./values/immich.values.yaml
+      '';
+
+      ".config/k3s/values/immich.values.yaml".text = ''
+        image:
+          tag: v1.106.1
+        immich:
+          persistence:
+            library:
+        postgresql:
+          enabled: true
+        resis:
+          enabled: true
+        machine-learning:
+          enabled: false
+      '';
+    };
+  };
 }
