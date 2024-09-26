c818282101e6ee9fd1d608431de0ae3d3f82808c
Michael Strobel <mstrobel97@gmail.com>
2024-09-26 23:59:53 +0200

    fix(hypr): syntax errors in screenshot script

--- bin/hypr-screenshot.sh
+++ bin/hypr-screenshot.sh
@@ -1,7 +1,8 @@
 clip=0
 area=0
 
-for arg in "$@" do
+for arg in "$@"
+do
   case $arg in
     clip) clip=1 ;;
     area) area=1 ;;
@@ -20,7 +21,7 @@ else
 
   filename="$dir/$(date +'%Y-%m-%d_%H:%M:%S_grim.png')"
 
-  if [ $area -eq 1]; then
+  if [ $area -eq 1 ]; then
     grim -g "$(slurp)" "$filename"
   else
     grim "$filename"
