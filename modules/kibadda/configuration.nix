8de6eeafea4c51a34d0a78534598d8ff5ce2ed30
Michael Strobel <mstrobel97@gmail.com>
2025-01-27 22:03:35 +0100

    refactor: order module imports

--- modules/kibadda/configuration.nix
+++ modules/kibadda/configuration.nix
@@ -1,12 +1,12 @@
 {
   imports = [
+    ../gaming/configuration.nix
+    ../home-office/configuration.nix
     ../hypr/configuration.nix
     ../i3/configuration.nix
+    ../nvidia/configuration.nix
+    ../vpn/configuration.nix
     ../yubikey/configuration.nix
     ../zsh/configuration.nix
-    ../nvidia/configuration.nix
-    ../home-office/configuration.nix
-    ../vpn/configuration.nix
-    ../gaming/configuration.nix
   ];
 }
--- modules/kibadda/home.nix
+++ modules/kibadda/home.nix
@@ -1,27 +1,28 @@
 { meta, pkgs, ... }:
 {
   imports = [
+    ../gaming/home.nix
+    ../home-office/home.nix
     ../hypr/home.nix
     ../i3/home.nix
-    ../zsh/home.nix
-    ../yubikey/home.nix
     ../nvidia/home.nix
-    ../home-office/home.nix
     ../vpn/home.nix
-    ../gaming/home.nix
-    ../git.nix
-    ../kitty.nix
+    ../yubikey/home.nix
+    ../zsh/home.nix
+
+    ../chrome.nix
+    ../cursor.nix
+    ../direnv.nix
     ../dunst.nix
     ../eza.nix
+    ../firefox.nix
+    ../font.nix
+    ../git.nix
+    ../kitty.nix
+    ../neovim.nix
     ../pass.nix
-    ../direnv.nix
     ../ssh.nix
     ../zoxide.nix
-    ../neovim.nix
-    ../firefox.nix
-    ../chrome.nix
-    ../font.nix
-    ../cursor.nix
   ];
 
   config = {
