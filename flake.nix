cb9476522a7f108ea8547234eb4934aec5fd2233
Michael Strobel <mstrobel97@gmail.com>
2025-01-28 22:59:50 +0100

    refactor: move basic home config to common

--- flake.nix
+++ flake.nix
@@ -89,28 +89,7 @@
             ./modules/kibadda/configuration.nix
             ./machines/${name}/configuration.nix
             home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit inputs meta; };
-              home-manager.users.${meta.username} = {
-                imports = [
-                  ./modules/kibadda/home.nix
-                  ./machines/${name}/home.nix
-                ];
-
-                # It is occasionally necessary for Home Manager to change configuration
-                # defaults in a way that is incompatible with stateful data. This could,
-                # for example, include switching the default data format or location of a file.
-                #
-                # The state version indicates which default settings are in effect and
-                # will therefore help avoid breaking program configurations. Switching
-                # to a higher state version typically requires performing some manual
-                # steps, such as data conversion or moving files.
-                home.stateVersion = "24.05";
-                programs.home-manager.enable = true;
-              };
-            }
+            ./machines/common/home.nix
           ];
         };
 
---
+++ machines/common/home.nix
@@ -0,0 +1,30 @@
+{
+  meta,
+  inputs,
+  ...
+}:
+{
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs meta; };
+    users.${meta.username} = {
+      imports = [
+        ../../modules/kibadda/home.nix
+        ../${meta.hostname}/home.nix
+      ];
+
+      programs.home-manager.enable = true;
+
+      # It is occasionally necessary for Home Manager to change configuration
+      # defaults in a way that is incompatible with stateful data. This could,
+      # for example, include switching the default data format or location of a file.
+      #
+      # The state version indicates which default settings are in effect and
+      # will therefore help avoid breaking program configurations. Switching
+      # to a higher state version typically requires performing some manual
+      # steps, such as data conversion or moving files.
+      home.stateVersion = "24.05";
+    };
+  };
+}
