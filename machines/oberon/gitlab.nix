f73d693c4263565cc4f99a7a47fe426d902aa367
Michael Strobel <mstrobel97@gmail.com>
2025-02-11 00:33:30 +0100

    feat(oberon): update gitlab settings

--- machines/oberon/gitlab.nix
+++ machines/oberon/gitlab.nix
@@ -6,21 +6,22 @@
 {
   services = {
     nginx.virtualHosts."git.${meta.pi.domain}" = {
-      # enableACME = true;
-      # forceSSL = true;
+      enableACME = true;
+      forceSSL = true;
       extraConfig = meta.pi.ip-whitelist;
       locations."/".proxyPass = "http://unix:/run/gitlab/gitlab-workhorse.socket";
     };
 
     gitlab = {
       enable = true;
-      databasePasswordFile = pkgs.writeText "dbPassword" "asdfasdfasdf";
-      initialRootPasswordFile = pkgs.writeText "rootPassword" "asdfasdfadf";
+      initialRootEmail = meta.pi.gitlab.admin.username;
+      initialRootPasswordFile = pkgs.writeText "rootPassword" meta.pi.gitlab.admin.password;
+      databasePasswordFile = pkgs.writeText "dbPassword" meta.pi.gitlab.db-password;
       secrets = {
-        secretFile = pkgs.writeText "secret" "ASDFJKLS";
-        otpFile = pkgs.writeText "otpsecret" "ASDFJKLS";
-        dbFile = pkgs.writeText "dbsecret" "ASDFJKLS";
-        jwsFile = pkgs.runCommand "oidcKeyBase" { } "${pkgs.openssl}/bin/openssl genrsa 2048 > $out";
+        secretFile = pkgs.writeText "secret" meta.pi.gitlab.secret;
+        otpFile = pkgs.writeText "otpsecret" meta.pi.gitlab.otp;
+        dbFile = pkgs.writeText "dbsecret" meta.pi.gitlab.db;
+        jwsFile = pkgs.writeText "jwssecret" meta.pi.gitlab.jws;
       };
     };
   };
