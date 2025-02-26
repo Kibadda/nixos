01eb0152219e97689b7338287f93aa8a7257934d
Michael Strobel <mstrobel97@gmail.com>
2025-02-26 22:58:25 +0100

    feat(oberon/home-assistant): update dashboard

--- machines/oberon/home-assistant.nix
+++ machines/oberon/home-assistant.nix
@@ -1,7 +1,6 @@
 {
   meta,
   pkgs,
-  lib,
   ...
 }:
 {
@@ -9,6 +8,7 @@
     nginx."${meta.pi.home-assistant.domain}" = {
       port = 8123;
       websockets = true;
+      restrict-access = true;
     };
 
     backup.home-assistant = {
@@ -29,10 +29,12 @@
         "sun"
         "google_translate"
         "met"
+        "radio_browser"
         "isal"
         "hue"
         "esphome"
         "default_config"
+        "frontend"
       ];
       customComponents = [
         (pkgs.home-assistant-custom-components.waste_collection_schedule.overrideAttrs (
@@ -49,9 +51,6 @@
       ];
       configDir = meta.pi.home-assistant.dir;
       config = {
-        default_config = { };
-        frontend = { };
-
         recorder = {
           db_url = "postgresql://@/hass";
         };
@@ -72,9 +71,11 @@
           longitude = meta.pi.home-assistant.lon;
           name = "Home";
           unit_system = "metric";
+          country = "DE";
+          language = "de";
         };
 
-        waste_collection_schedult = {
+        waste_collection_schedule = {
           sources = [
             {
               name = "jumomind_de";
@@ -90,8 +91,98 @@
         sensor = [
           {
             platform = "waste_collection_schedule";
-            name = "Müll";
-            value_template = "{{value.types|join(\", \")}}|{{value.daysTo}}|{{value.date.strftime(\"%d.%m.%Y\")}}|{{value.date.strftime(\"%a\")}}";
+            name = "Blaue Tonne";
+            value_template = "{% if value.daysTo == 0 %}heute{% elif value.daysTo == 1%}morgen{% else %}in {{ value.daysTo }} Tagen{% endif %}";
+            date_template = "{{ value.date.strftime('%a, %d.%m.%Y') }}";
+            types = [ "Blaue Tonne" ];
+            add_days_to = true;
+          }
+          {
+            platform = "waste_collection_schedule";
+            name = "Gelber Sack";
+            value_template = "{% if value.daysTo == 0 %}heute{% elif value.daysTo == 1%}morgen{% else %}in {{ value.daysTo }} Tagen{% endif %}";
+            date_template = "{{ value.date.strftime('%a, %d.%m.%Y') }}";
+            types = [ "Gelber Sack" ];
+            add_days_to = true;
+          }
+          {
+            platform = "waste_collection_schedule";
+            name = "Biomüll";
+            value_template = "{% if value.daysTo == 0 %}heute{% elif value.daysTo == 1%}morgen{% else %}in {{ value.daysTo }} Tagen{% endif %}";
+            date_template = "{{ value.date.strftime('%a, %d.%m.%Y') }}";
+            types = [ "Biomüll" ];
+            add_days_to = true;
+          }
+          {
+            platform = "waste_collection_schedule";
+            name = "Restmüll";
+            value_template = "{% if value.daysTo == 0 %}heute{% elif value.daysTo == 1%}morgen{% else %}in {{ value.daysTo }} Tagen{% endif %}";
+            date_template = "{{ value.date.strftime('%a, %d.%m.%Y') }}";
+            types = [ "Restmüll" ];
+            add_days_to = true;
+          }
+        ];
+      };
+      lovelaceConfig = {
+        title = "Dashboard";
+        views = [
+          {
+            title = "Dashboard";
+            cards = [
+              {
+                type = "entities";
+                title = "Müll";
+                entities = [
+                  {
+                    entity = "sensor.gelber_sack";
+                    name = "Gelber Sack";
+                  }
+                  {
+                    entity = "sensor.biomull";
+                    name = "Biomüll";
+                  }
+                  {
+                    entity = "sensor.restmull";
+                    name = "Restmüll";
+                  }
+                  {
+                    entity = "sensor.blaue_tonne";
+                    name = "Blaue Tonne";
+                  }
+                ];
+              }
+              {
+                entity = "weather.forecast_home";
+                forecast_type = "daily";
+                type = "weather-forecast";
+              }
+              {
+                type = "tile";
+                entity = "light.arbeitszimmer";
+                name = "Arbeitszimmer";
+                features = [
+                  { type = "light-brightness"; }
+                  { type = "light-color-temp"; }
+                ];
+              }
+              {
+                type = "tile";
+                entity = "light.esszimmer";
+                name = "Esszimmer";
+                features = [
+                  { type = "light-brightness"; }
+                ];
+              }
+              {
+                type = "tile";
+                entity = "light.wohnzimmer";
+                name = "Wohnzimmer";
+                features = [
+                  { type = "light-brightness"; }
+                  { type = "light-color-temp"; }
+                ];
+              }
+            ];
           }
         ];
       };
@@ -112,6 +203,5 @@
     serviceConfig = {
       PrivateDevices = true;
     };
-    reloadTriggers = lib.mkForce [ ];
   };
 }
