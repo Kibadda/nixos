beee40b9729f6dbff8d75e5c336ad37e4f9d48bb
Michael Strobel <mstrobel97@gmail.com>
2025-02-26 13:57:03 +0100

    feat(oberon/home-assistant): trying out more complicated configuration

--- machines/oberon/home-assistant.nix
+++ machines/oberon/home-assistant.nix
@@ -1,5 +1,7 @@
 {
   meta,
+  pkgs,
+  lib,
   ...
 }:
 {
@@ -15,38 +17,101 @@
     };
   };
 
-  services.home-assistant = {
-    enable = true;
-    extraComponents = [
-      "analytics"
-      "google_translate"
-      "met"
-      "radio_browser"
-      "shopping_list"
-      "isal"
-      "hue"
-      "esphome"
-      "default_config"
-      "wled"
-    ];
-    configDir = meta.pi.home-assistant.dir;
-    config = {
-      default_config = { };
-      http = {
-        server_port = 8123;
-        base_url = "https://${meta.pi.home-assistant.domain}";
-        use_x_forwarded_for = true;
-        cors_allowed_origins = "*";
-        trusted_proxies = [
-          "127.0.0.1"
+  services = {
+    home-assistant = {
+      enable = true;
+      package = pkgs.home-assistant.override {
+        extraPackages = ps: [
+          ps.psycopg2 # postgres support
         ];
       };
-      homeassistant = {
-        latitude = meta.pi.home-assistant.lat;
-        longitude = meta.pi.home-assistant.lon;
-        name = "Home";
-        unit_system = "metric";
+      extraComponents = [
+        "sun"
+        "google_translate"
+        "met"
+        "isal"
+        "hue"
+        "esphome"
+        "default_config"
+      ];
+      customComponents = [
+        (pkgs.home-assistant-custom-components.waste_collection_schedule.overrideAttrs (
+          final: _: {
+            version = "2.6.0";
+            src = pkgs.fetchFromGitHub {
+              owner = "mampfes";
+              repo = "hacs_waste_collection_schedule";
+              tag = final.version;
+              hash = "sha256-gfL5Nxe8io7DTya5x8aQ5PhxiH8rb8L3/CA+UqKEDAk=";
+            };
+          }
+        ))
+      ];
+      configDir = meta.pi.home-assistant.dir;
+      config = {
+        default_config = { };
+        frontend = { };
+
+        recorder = {
+          db_url = "postgresql://@/hass";
+        };
+
+        http = {
+          server_port = 8123;
+          base_url = "https://${meta.pi.home-assistant.domain}";
+          use_x_forwarded_for = true;
+          cors_allowed_origins = "*";
+          trusted_proxies = [
+            "127.0.0.1"
+            "::1"
+          ];
+        };
+
+        homeassistant = {
+          latitude = meta.pi.home-assistant.lat;
+          longitude = meta.pi.home-assistant.lon;
+          name = "Home";
+          unit_system = "metric";
+        };
+
+        waste_collection_schedult = {
+          sources = [
+            {
+              name = "jumomind_de";
+              args = {
+                service_id = "mymuell";
+                city = meta.pi.home-assistant.city;
+                street = meta.pi.home-assistant.street;
+              };
+            }
+          ];
+        };
+
+        sensor = [
+          {
+            platform = "waste_collection_schedule";
+            name = "Müll";
+            value_template = "{{value.types|join(\", \")}}|{{value.daysTo}}|{{value.date.strftime(\"%d.%m.%Y\")}}|{{value.date.strftime(\"%a\")}}";
+          }
+        ];
       };
     };
+
+    postgresql = {
+      ensureDatabases = [ "hass" ];
+      ensureUsers = [
+        {
+          name = "hass";
+          ensureDBOwnership = true;
+        }
+      ];
+    };
+  };
+
+  systemd.services.home-assistant = {
+    serviceConfig = {
+      PrivateDevices = true;
+    };
+    reloadTriggers = lib.mkForce [ ];
   };
 }
