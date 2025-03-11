346b4717ee5cca6d7ff88f28a4d5dcdb208da5eb
Michael Strobel <mstrobel97@gmail.com>
2025-03-12 00:16:40 +0100

    feat(uranus): add services for triggering hue webhooks

--- machines/uranus/configuration.nix
+++ machines/uranus/configuration.nix
@@ -1,4 +1,6 @@
 {
+  meta,
+  pkgs,
   ...
 }:
 {
@@ -9,4 +11,31 @@
 
     ../../modules/kibadda/configuration.nix
   ];
+
+  systemd.services = {
+    hue-arbeitszimmer-on = {
+      description = "Turn on lights in arbeitszimmer";
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.curl}/bin/curl -X PUT https://${meta.pi.home-assistant.domain}/api/webhook/${meta.pi.home-assistant.webhooks.uranus.light_on}";
+      };
+    };
+
+    hue-arbeitszimmer-off = {
+      description = "Turn off lights in arbeitszimmer";
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "network-online.target" ];
+      after = [
+        "network-online.target"
+        "final.target"
+      ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.curl}/bin/curl -X PUT https://${meta.pi.home-assistant.domain}/api/webhook/${meta.pi.home-assistant.webhooks.uranus.light_off}";
+      };
+    };
+  };
 }
