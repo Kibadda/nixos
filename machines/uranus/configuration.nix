7a9a8e435db25c4540d47b02eb37c9a7b31a9206
Michael Strobel <mstrobel97@gmail.com>
2025-03-14 10:00:31 +0100

    fix(uranus): shutdown service not running

--- machines/uranus/configuration.nix
+++ machines/uranus/configuration.nix
@@ -28,11 +28,13 @@
     state-uranus-off = {
       enable = true;
       description = "Update uranus state on shutdown";
-      wants = [ "network-online.target" ];
+      wantedBy = [ "shutdown.target" ];
       before = [ "shutdown.target" ];
       serviceConfig = {
         Type = "oneshot";
         ExecStart = "${pkgs.curl}/bin/curl -X PUT https://${meta.pi.home-assistant.domain}/api/webhook/${meta.pi.home-assistant.webhooks.uranus.shutdown}";
+        TimeoutStartSec = 0;
+        RemainAfterExit = "yes";
       };
     };
   };
