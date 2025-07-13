b55b9cf6e983af610b2b68bf0c46254c3518fe3d
Michael Strobel <mstrobel97@gmail.com>
2025-07-13 22:14:13 +0200

    fix(uranus): disable boot and shutdown home-assistant webhooks

--- machines/uranus/configuration.nix
+++ machines/uranus/configuration.nix
@@ -12,30 +12,30 @@
     ../../modules/kibadda/configuration.nix
   ];
 
-  systemd.services = {
-    state-uranus-on = {
-      enable = true;
-      description = "Update uranus state on startup";
-      wantedBy = [ "multi-user.target" ];
-      wants = [ "network-online.target" ];
-      after = [ "network-online.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.curl}/bin/curl -X PUT https://${meta.pi.home-assistant.domain}/api/webhook/${meta.pi.home-assistant.webhooks.uranus.startup}";
-      };
-    };
-
-    state-uranus-off = {
-      enable = true;
-      description = "Update uranus state on shutdown";
-      wantedBy = [ "shutdown.target" ];
-      before = [ "shutdown.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.curl}/bin/curl -X PUT https://${meta.pi.home-assistant.domain}/api/webhook/${meta.pi.home-assistant.webhooks.uranus.shutdown}";
-        TimeoutStartSec = 0;
-        RemainAfterExit = "yes";
-      };
-    };
-  };
+  # systemd.services = {
+  #   state-uranus-on = {
+  #     enable = true;
+  #     description = "Update uranus state on startup";
+  #     wantedBy = [ "multi-user.target" ];
+  #     wants = [ "network-online.target" ];
+  #     after = [ "network-online.target" ];
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       ExecStart = "${pkgs.curl}/bin/curl -X PUT https://${meta.pi.home-assistant.domain}/api/webhook/${meta.pi.home-assistant.webhooks.uranus.startup}";
+  #     };
+  #   };
+  #
+  #   state-uranus-off = {
+  #     enable = true;
+  #     description = "Update uranus state on shutdown";
+  #     wantedBy = [ "shutdown.target" ];
+  #     before = [ "shutdown.target" ];
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       ExecStart = "${pkgs.curl}/bin/curl -X PUT https://${meta.pi.home-assistant.domain}/api/webhook/${meta.pi.home-assistant.webhooks.uranus.shutdown}";
+  #       TimeoutStartSec = 0;
+  #       RemainAfterExit = "yes";
+  #     };
+  #   };
+  # };
 }
