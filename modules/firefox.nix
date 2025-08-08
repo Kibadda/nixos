782ea0c534851052444d1a508105450be1876345
Michael Strobel <mstrobel97@gmail.com>
2025-08-08 13:48:02 +0200

    feat(firefox): add to default applications

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -28,6 +28,18 @@ in
       BROWSER = "firefox";
     };
 
+    xdg.mimeApps = lib.mkIf (cfg.firefox.default || !cfg.chrome.enable) {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/unknown" = "firefox.desktop";
+        "application/pdf" = "firefox.desktop";
+      };
+    };
+
     programs.firefox = {
       enable = true;
       policies = {
