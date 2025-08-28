d05235bc2099efaeae437683b0b550fa665eb3bb
Michael Strobel <mstrobel97@gmail.com>
2025-08-29 00:20:38 +0200

    feat(oberon/mealie): add sso via authelia

--- machines/oberon/mealie.nix
+++ machines/oberon/mealie.nix
@@ -13,7 +13,25 @@
       path = secrets.pi.mealie.dir;
       time = "03:30";
     };
+
+    authelia.mealie = {
+      secret = secrets.pi.authelia.oidc.mealie;
+      redirect_uris = [
+        "https://${secrets.pi.mealie.domain}/login"
+      ];
+    };
   };
 
-  services.mealie.enable = true;
+  services.mealie = {
+    enable = true;
+    settings = {
+      ALLOW_PASSWORD_LOGIN = false;
+      OIDC_AUTH_ENABLED = "true";
+      OIDC_CONFIGURATION_URL = "https://${secrets.pi.authelia.domain}/.well-known/openid-configuration";
+      OIDC_CLIENT_ID = "mealie";
+      OIDC_CLIENT_SECRET = secrets.pi.authelia.oidc.mealie;
+      OIDC_AUTO_REDIRECT = true;
+      OIDC_PROVIDER_NAME = "Authelia";
+    };
+  };
 }
