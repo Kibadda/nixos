1de487153932ff423a2bbf05dc2e8a67d8072d7f
Michael Strobel <mstrobel97@gmail.com>
2025-09-15 14:46:14 +0200

    fix(immich): increase max body size

--- machines/oberon/immich.nix
+++ machines/oberon/immich.nix
@@ -9,7 +9,7 @@
       port = 2283;
       websockets = true;
       extraConfig = ''
-        client_max_body_size 50000M;
+        client_max_body_size 100000M;
         proxy_read_timeout 600s;
         proxy_send_timeout 600s;
         send_timeout 600s;
