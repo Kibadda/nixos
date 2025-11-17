253f43e4be5fa187cf15f5553119d072038e75bb
Michael Strobel <mstrobel97@gmail.com>
2025-11-18 00:49:20 +0100

    feat(ophelia): use gnome instead of hyprland

--- machines/ophelia/configuration.nix
+++ machines/ophelia/configuration.nix
@@ -1,6 +1,5 @@
 {
   inputs,
-  secrets,
   pkgs,
   ...
 }:
@@ -14,26 +13,9 @@
     ../../modules/kibadda/configuration.nix
   ];
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "Hyprland";
-        user = secrets.base.username;
-      };
-    };
-  };
-
   environment.systemPackages = with pkgs; [
     playerctl
     brightnessctl
     pamixer
   ];
-
-  networking = {
-    wireless = {
-      enable = true;
-      networks.${secrets.home.wifi."2.4"}.psk = secrets.home.wifi.pass;
-    };
-  };
 }
--- machines/ophelia/home.nix
+++ machines/ophelia/home.nix
@@ -1,19 +1,9 @@
 {
-  pkgs,
   ...
 }:
 {
   imports = [
     ../../modules/kibadda/home.nix
-  ];
-
-  # TODO
-  # - keyboard?
-  # - mouse support?
-  # - hyprgrass
-
-  home.packages = [
-    pkgs.kibadda.ophelia-landscape-toggle
   ];
 
   kibadda = {
@@ -23,65 +13,6 @@
 
     yubikey.enable = false;
 
-    hypr = {
-      enable = true;
-
-      monitor = [
-        "DSI-1, 1080x2280@60, 0x0, 1, transform, 1"
-      ];
-
-      device = [
-        {
-          name = "synaptics-s3706b";
-          transform = 1;
-        }
-      ];
-
-      bind = [
-        "SUPER, m, exec, ${pkgs.kibadda.ophelia-landscape-toggle}/bin/ophelia-landscape-toggle"
-        "SUPER SHIFT, Down, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 10%-"
-        "SUPER SHIFT, Up, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 10%+"
-      ];
-
-      # FIX: make this work again
-      hyprpaper.enable = false;
-      hyprlock.enable = false;
-      hypridle.enable = false;
-
-      waybar = {
-        top = {
-          modules-left = [ "hyprland/workspaces" ];
-          modules-right = [
-            "battery"
-            "backlight"
-            # FIX: sound not working
-            # "pulseaudio"
-            "network"
-          ];
-        };
-        bottom = {
-          modules-left = [ "bluetooth" ];
-          modules-center = [
-            "cpu"
-            "memory"
-            "disk"
-          ];
-          modules-right = [ "clock" ];
-        };
-
-        extraCss =
-          let
-            margin = "50px";
-          in
-          ''
-            .modules-left {
-              margin-left: ${margin};
-            }
-            .modules-right {
-              margin-right: ${margin};
-            }
-          '';
-      };
-    };
+    gnome.enable = true;
   };
 }
