9f6eeeb0656f5892bf607338a7571821512fb92c
Michael Strobel <mstrobel97@gmail.com>
2026-01-09 13:26:18 +0100

    fix(i3): remove superfluous module

--- modules/kibadda/configuration.nix
+++ modules/kibadda/configuration.nix
@@ -3,7 +3,6 @@
     ../gaming/configuration.nix
     ../office/configuration.nix
     ../hypr/configuration.nix
-    ../i3/configuration.nix
     ../nvidia/configuration.nix
     ../vpn/configuration.nix
     ../yubikey/configuration.nix
