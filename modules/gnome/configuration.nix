393c59d1b9266a857978db66c8b20ed728d17314
Michael Strobel <mstrobel97@gmail.com>
2026-01-13 11:34:50 +0100

    feat(gnome): add keybinds for nautilus and navigation

--- modules/gnome/configuration.nix
+++ modules/gnome/configuration.nix
@@ -178,6 +178,7 @@ in
 
                 "org/gnome/settings-daemon/plugins/media-keys" = {
                   www = [ "<Super>b" ];
+                  home = [ "<Super>f" ];
                   next = [ "AudioNext" ];
                   play = [ "AudioPlay" ];
                   previous = [ "AudioPrev" ];
@@ -190,18 +191,30 @@ in
                   screensaver = [ "<Shift><Super>s" ];
                 };
 
+                "org/gnome/nautilus/preferences" = {
+                  date-time-format = "detailed";
+                };
+
+                "org/gnome/nautilus/window-state" = {
+                  maximized = true;
+                };
+
                 "org/gnome/desktop/wm/keybindings" = {
                   close = [ "<Super>Q" ];
                   move-to-workspace-1 = [ "<Shift><Super>1" ];
                   move-to-workspace-2 = [ "<Shift><Super>2" ];
                   move-to-workspace-3 = [ "<Shift><Super>3" ];
                   move-to-workspace-4 = [ "<Shift><Super>4" ];
+                  move-to-workspace-left = [ "<Shift><Super>d" ];
+                  move-to-workspace-right = [ "<Shift><Super>u" ];
                   move-to-monitor-left = [ "<Shift><Super>h" ];
                   move-to-monitor-right = [ "<Shift><Super>l" ];
                   switch-to-workspace-1 = [ "<Super>1" ];
                   switch-to-workspace-2 = [ "<Super>2" ];
                   switch-to-workspace-3 = [ "<Super>3" ];
                   switch-to-workspace-4 = [ "<Super>4" ];
+                  switch-to-workspace-left = [ "<Super>d" ];
+                  switch-to-workspace-right = [ "<Super>u" ];
                   switch-windows = [ "<Super>Tab" ];
                   switch-windows-backward = [ "<Shift><Super>Tab" ];
                   toggle-fullscreen = [ "<Shift><Super>space" ];
