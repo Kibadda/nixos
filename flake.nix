620092a021766fb6ef72150b175e064ee32793ce
Michael Strobel <mstrobel97@gmail.com>
2026-03-18 10:49:08 +0100

    feat(dendritic): update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,115 +1,45 @@
 {
-  description = "Top level NixOS Flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     server-nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    unstable-nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    linphone-nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-
-    mobile-nixpkgs.url = "github:nixos/nixpkgs?rev=f02fddb8acef29a8b32f10a335d44828d7825b78";
-    mobile-nixos = {
-      url = "github:mobile-nixos/mobile-nixos";
-      flake = false;
-    };
-
-    nur = {
-      url = "github:nix-community/NUR";
+    disko = {
+      url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-monitored = {
       url = "github:ners/nix-monitored";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    powermenu.url = "github:Kibadda/powermenu";
-    dmenu.url = "github:Kibadda/dmenu";
-    passmenu.url = "github:Kibadda/passmenu";
-    pinentry.url = "github:Kibadda/adw-pinentry";
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     work.url = "github:Kibadda/work";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware.url = "github:nixos/nixos-hardware";
   };
 
   outputs =
-    { self, ... }@inputs:
+    inputs:
     let
       secrets = {
         base = import ./secrets/base.nix;
-        home = import ./secrets/home.nix;
         pi = import ./secrets/pi.nix;
+        home = import ./secrets/home.nix;
         work = import ./secrets/work.nix;
       };
-
-      nixosSystem =
-        {
-          hostname,
-          system ? "x86_64-linux",
-          nixpkgs ? inputs.nixpkgs,
-        }:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs secrets hostname;
-          };
-          modules = [ ./machines/${hostname}/configuration.nix ];
-        };
     in
-    {
-      nixosConfigurations = {
-        # Home PC
-        uranus = nixosSystem {
-          hostname = "uranus";
-        };
-        # Laptop
-        titania = nixosSystem {
-          hostname = "titania";
-        };
-        # Work
-        setebos = nixosSystem {
-          hostname = "setebos";
-        };
-        # Raspberry Pi 1
-        oberon = nixosSystem {
-          hostname = "oberon";
-          system = "aarch64-linux";
-          nixpkgs = inputs.server-nixpkgs;
-        };
-        # Raspberry Pi 2
-        umbriel = nixosSystem {
-          hostname = "umbriel";
-          system = "aarch64-linux";
-          nixpkgs = inputs.server-nixpkgs;
-        };
-        # OnePlus 6
-        # ophelia = nixosSystem {
-        #   hostname = "ophelia";
-        #   system = "aarch64-linux";
-        #   nixpkgs = inputs.mobile-nixpkgs;
-        # };
-      };
-
-      devShells =
-        let
-          system = "x86_64-linux";
-        in
-        {
-          ${system}.default = inputs.nixpkgs.legacyPackages.${system}.mkShell {
-            name = "nixos";
-          };
-        };
-    };
+    inputs.flake-parts.lib.mkFlake {
+      inherit inputs;
+      specialArgs = { inherit secrets; };
+    } (inputs.import-tree ./modules);
 }
