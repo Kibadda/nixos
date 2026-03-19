1873a4a855fc5d24d0f2576a7940ecc920574a1f
Michael Strobel <mstrobel97@gmail.com>
2026-03-19 08:12:37 +0100

    feat(dendritic): convert trilium module

--- modules/services/trilium.nix
+++ modules/services/trilium.nix
@@ -1,51 +1,51 @@
 {
   secrets,
-  pkgs,
   ...
 }:
 {
-  oberon = {
-    nginx.${secrets.pi.trilium.domain} = {
-      restrict-access = false;
-      websockets = true;
-      port = 8084;
+  flake.nixosModules.trilium =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      kibadda.services.trilium = {
+        description = "Notizen";
+        subdomain = "notes";
+        port = 8084;
+        open = true;
+        auth = "oidc";
+        oidc = {
+          redirect_uris = [
+            "https://notes.${secrets.pi.domain}/callback"
+          ];
+          method = "basic";
+          claim = {
+            id_token = [
+              "email"
+              "name"
+            ];
+          };
+        };
+        backup = {
+          paths = [ "/mnt/trilium" ];
+          time = "03:30";
+        };
+      };
+
+      services.trilium-server = {
+        enable = true;
+        port = 8084;
+        noBackup = true;
+        environmentFile = pkgs.writeText "trilium.env" ''
+          TRILIUM_OAUTH_BASE_URL=https://notes.${secrets.pi.domain}
+          TRILIUM_OAUTH_CLIENT_ID=trilium
+          TRILIUM_OAUTH_CLIENT_SECRET=${secrets.pi.authelia.oidc.trilium}
+          TRILIUM_OAUTH_ISSUER_BASE_URL=https://sso.${secrets.pi.domain}
+          TRILIUM_OAUTH_ISSUER_NAME=Authelia
+          TRILIUM_OAUTH_ISSUER_ICON=https://www.authelia.com/images/branding/logo-cropped.png
+        '';
+        dataDir = "/mnt/trilium";
+      };
     };
-
-    backup.trilium = {
-      path = secrets.pi.trilium.dir;
-      time = "03:30";
-    };
-
-    authelia.trilium = {
-      secret = secrets.pi.authelia.oidc.trilium;
-      redirect_uris = [
-        "https://${secrets.pi.trilium.domain}/callback"
-      ];
-      policy = "trilium";
-    };
-
-    dashboard.Home = [
-      {
-        name = "Trilium";
-        icon = "trilium.svg";
-        description = "Notes";
-        url = "https://${secrets.pi.trilium.domain}";
-      }
-    ];
-  };
-
-  services.trilium-server = {
-    enable = true;
-    port = 8084;
-    noBackup = true;
-    environmentFile = pkgs.writeText "trilium.env" ''
-      TRILIUM_OAUTH_BASE_URL=https://${secrets.pi.trilium.domain}
-      TRILIUM_OAUTH_CLIENT_ID=trilium
-      TRILIUM_OAUTH_CLIENT_SECRET=${secrets.pi.authelia.oidc.trilium}
-      TRILIUM_OAUTH_ISSUER_BASE_URL=https://${secrets.pi.authelia.domain}
-      TRILIUM_OAUTH_ISSUER_NAME=Authelia
-      TRILIUM_OAUTH_ISSUER_ICON=https://www.authelia.com/images/branding/logo-cropped.png
-    '';
-    dataDir = secrets.pi.trilium.dir;
-  };
 }
