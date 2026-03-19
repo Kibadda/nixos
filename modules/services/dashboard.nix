32b918f8367a8b7b466261a7cec2ac48caf5aa65
Michael Strobel <mstrobel97@gmail.com>
2026-03-19 08:09:33 +0100

    feat(dendritic): convert dashboard module

--- modules/services/dashboard.nix
+++ modules/services/dashboard.nix
@@ -1,124 +1,72 @@
 {
-  secrets,
-  config,
   lib,
+  secrets,
   ...
 }:
-let
-  cfg = config.home-manager.users.${secrets.base.username}.kibadda;
-in
 {
-  options =
-    let
-      dashboardService = {
-        options = {
-          name = lib.mkOption {
-            type = lib.types.str;
-          };
-          icon = lib.mkOption {
-            type = lib.types.str;
-          };
-          description = lib.mkOption {
-            type = lib.types.str;
-          };
-          url = lib.mkOption {
-            type = lib.types.str;
-          };
-        };
-      };
-    in
+  flake.nixosModules.dashboard =
     {
-      oberon.dashboard = {
-        Home = lib.mkOption {
-          type = lib.types.listOf (lib.types.submodule dashboardService);
-          default = [ ];
-        };
-        Coding = lib.mkOption {
-          type = lib.types.listOf (lib.types.submodule dashboardService);
-          default = [ ];
-        };
-      };
-    };
-
-  config = {
-    oberon = {
-      nginx.${secrets.pi.dashboard.domain} = {
-        restrict-access = true;
-        port = 8082;
-      };
-    };
-
-    services.homepage-dashboard = {
-      enable = true;
-      environmentFile = builtins.toFile "homepage.env" "HOMEPAGE_ALLOWED_HOSTS=${secrets.pi.dashboard.domain}";
-      customCSS = ''
-        body, html {
-          font-family: ${cfg.font.main.name} Nerd Font, Helvetica, Arial, sans-serif !important;
-        }
-        .font-medium {
-          font-weight: 700 !important;
-        }
-        .font-light {
-          font-weight: 500 !important;
-        }
-        .font-thin {
-          font-weight: 400 !important;
-        }
-        #information-widgets {
-          padding-left: 1.5rem;
-          padding-right: 1.5rem;
-        }
-        div#footer {
-          display: none;
-        }
-        .services-group.basis-full.flex-1.px-1.-my-1 {
-          padding-bottom: 3rem;
-        };
-      '';
-      settings = {
-        layout = [
-          {
-            Home = {
-              header = true;
-              style = "column";
-            };
+      config,
+      ...
+    }:
+    {
+      services.homepage-dashboard = {
+        enable = true;
+        allowedHosts = "dashboard.${secrets.pi.domain}";
+        customCSS = ''
+          body, html {
+            font-family: JetBrainsMono Nerd Font, Helvetica, Arial, sans-serif !important;
           }
-          {
-            Coding = {
-              header = true;
-              style = "column";
-            };
+          .font-medium {
+            font-weight: 700 !important;
           }
-          {
-            Links = {
-              header = true;
-              style = "column";
-            };
+          .font-light {
+            font-weight: 500 !important;
           }
-        ];
-        headerStyle = "clean";
-        statusStyle = "dot";
-        hideVersion = "true";
-      };
-      services =
-        let
-          convertServices =
-            services:
-            builtins.map (conf: {
-              ${conf.name} = {
-                icon = conf.icon;
-                description = conf.description;
-                href = conf.url;
-                siteMonitor = conf.url;
+          .font-thin {
+            font-weight: 400 !important;
+          }
+          #information-widgets {
+            padding-left: 1.5rem;
+            padding-right: 1.5rem;
+          }
+          div#footer {
+            display: none;
+          }
+          .services-group.basis-full.flex-1.px-1.-my-1 {
+            padding-bottom: 3rem;
+          };
+        '';
+        settings = {
+          layout = [
+            {
+              Services = {
+                header = true;
+                style = "column";
               };
-            }) services;
-        in
-        [
+            }
+            {
+              Links = {
+                header = true;
+                style = "column";
+              };
+            }
+          ];
+          headerStyle = "clean";
+          statusStyle = "dot";
+          hideVersion = "true";
+        };
+        services = [
           {
-            Home = convertServices config.oberon.dashboard.Home;
-          }
-          {
-            Coding = convertServices config.oberon.dashboard.Coding;
+            Services = lib.concatMap (service: [
+              {
+                ${service.description} = {
+                  icon = "${service.name}.svg";
+                  href = service.url;
+                  siteMonitor = service.url;
+                };
+              }
+            ]) (builtins.attrValues config.kibadda.services);
           }
           {
             Links = [
@@ -155,6 +103,6 @@ in
             ];
           }
         ];
+      };
     };
-  };
 }
