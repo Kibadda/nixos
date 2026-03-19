ea65cedb7cf4b337aaf908155fd7a14959c333c7
Michael Strobel <mstrobel97@gmail.com>
2026-03-19 08:00:08 +0100

    feat(dendritic): convert work-helper package

--- modules/packages/work-helper.nix
+++ modules/packages/work-helper.nix
@@ -1,55 +1,61 @@
 {
-  pkgs,
-}:
-pkgs.writeShellApplication {
-  name = "w";
-  runtimeInputs = [
-    pkgs.sshfs
-    pkgs.sftpman
-  ];
-  text = ''
-    work_vpn() {
-      if [ "$1" == "up" ]; then
-        wgctl work up
-      elif [ "$1" == "down" ]; then
-        wgctl work down
-      fi
-    }
+  perSystem =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      packages.work-helper = pkgs.writeShellApplication {
+        name = "w";
+        runtimeInputs = [
+          pkgs.sshfs
+          pkgs.sftpman
+        ];
+        text = ''
+          work_vpn() {
+            if [ "$1" == "up" ]; then
+              wgctl work up
+            elif [ "$1" == "down" ]; then
+              wgctl work down
+            fi
+          }
 
-    work_sshfs() {
-      if [ "$1" == "up" ]; then
-        sftpman mount studies
-        sftpman mount in
-      elif [ "$1" == "down" ]; then
-        sftpman umount studies
-        sftpman umount in
-      fi
-    }
+          work_sshfs() {
+            if [ "$1" == "up" ]; then
+              sftpman mount studies
+              sftpman mount in
+            elif [ "$1" == "down" ]; then
+              sftpman umount studies
+              sftpman umount in
+            fi
+          }
 
-    work_smb() {
-      if [ "$1" == "up" ]; then
-        sudo mount -t cifs -o credentials=~/.smbcredentials,uid=1000,gid=1000 //192.168.22.3/team /mnt/team
-        sudo mount -t cifs -o credentials=~/.smbcredentials,uid=1000,gid=1000 //192.168.22.3/temp /mnt/temp
-      elif [ "$1" == "down" ]; then
-        sudo umount /mnt/team
-        sudo umount /mnt/temp
-      fi
-    }
+          work_smb() {
+            if [ "$1" == "up" ]; then
+              sudo mount -t cifs -o credentials=~/.smbcredentials,uid=1000,gid=1000 //192.168.22.3/team /mnt/team
+              sudo mount -t cifs -o credentials=~/.smbcredentials,uid=1000,gid=1000 //192.168.22.3/temp /mnt/temp
+            elif [ "$1" == "down" ]; then
+              sudo umount /mnt/team
+              sudo umount /mnt/temp
+            fi
+          }
 
-    if [ "$1" == "up" ]; then
-      work_vpn up
-      work_sshfs up
-      work_smb up
-    elif [ "$1" == "down" ]; then
-      work_smb down
-      work_sshfs down
-      work_vpn down
-    elif [ "$1" == "vpn" ]; then
-      work_vpn "$2"
-    elif [ "$1" == "sshfs" ]; then
-      work_sshfs "$2"
-    elif [ "$1" == "smb" ]; then
-      work_smb "$2"
-    fi
-  '';
+          if [ "$1" == "up" ]; then
+            work_vpn up
+            work_sshfs up
+            work_smb up
+          elif [ "$1" == "down" ]; then
+            work_smb down
+            work_sshfs down
+            work_vpn down
+          elif [ "$1" == "vpn" ]; then
+            work_vpn "$2"
+          elif [ "$1" == "sshfs" ]; then
+            work_sshfs "$2"
+          elif [ "$1" == "smb" ]; then
+            work_smb "$2"
+          fi
+        '';
+      };
+    };
 }
