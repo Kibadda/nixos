521770b9fd7dbd3fd591c22ac35a97fd9db09224
Michael Strobel <mstrobel97@gmail.com>
2026-03-19 08:03:28 +0100

    feat(dendritic): convert ssh module

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -1,20 +1,15 @@
 {
-  config,
   lib,
   ...
 }:
-let
-  cfg = config.kibadda;
-in
 {
-  options = {
-    kibadda.ssh = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-      };
-
-      hosts = lib.mkOption {
+  flake.homeModules.ssh =
+    {
+      config,
+      ...
+    }:
+    {
+      options.kibadda.ssh.hosts = lib.mkOption {
         type = lib.types.listOf (
           lib.types.submodule {
             options = {
@@ -51,52 +46,51 @@ in
         );
         default = [ ];
       };
-    };
-  };
 
-  config = lib.mkIf cfg.ssh.enable {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      matchBlocks =
-        let
-          blocks = builtins.listToAttrs (
-            map (config: {
-              name = config.name;
-              value = {
-                hostname = config.host;
-                port = config.port;
-                user = config.user;
-                identityFile = lib.mkIf (config.identity != null) "~/.ssh/${config.identity}";
-                remoteForwards = lib.mkIf config.forward [
-                  {
-                    bind.address = "/run/user/1000/gnupg/S.gpg-agent.ssh";
-                    host.address = "/run/user/1000/gnupg/S.gpg-agent.ssh";
-                  }
-                  {
-                    bind.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
-                    host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
-                  }
-                ];
+      config = {
+        programs.ssh = {
+          enable = true;
+          enableDefaultConfig = false;
+          matchBlocks =
+            let
+              blocks = builtins.listToAttrs (
+                map (config: {
+                  name = config.name;
+                  value = {
+                    hostname = config.host;
+                    port = config.port;
+                    user = config.user;
+                    identityFile = lib.mkIf (config.identity != null) "~/.ssh/${config.identity}";
+                    remoteForwards = lib.mkIf config.forward [
+                      {
+                        bind.address = "/run/user/1000/gnupg/S.gpg-agent.ssh";
+                        host.address = "/run/user/1000/gnupg/S.gpg-agent.ssh";
+                      }
+                      {
+                        bind.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
+                        host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
+                      }
+                    ];
+                  };
+                }) config.kibadda.ssh.hosts
+              );
+            in
+            blocks
+            // {
+              "*" = {
+                forwardAgent = false;
+                addKeysToAgent = "no";
+                compression = false;
+                serverAliveInterval = 0;
+                serverAliveCountMax = 3;
+                hashKnownHosts = false;
+                userKnownHostsFile = "~/.ssh/known_hosts";
+                controlMaster = "no";
+                controlPath = "~/.ssh/master-%r@%n:%p";
+                controlPersist = "no";
               };
-            }) cfg.ssh.hosts
-          );
-        in
-        blocks
-        // {
-          "*" = {
-            forwardAgent = false;
-            addKeysToAgent = "no";
-            compression = false;
-            serverAliveInterval = 0;
-            serverAliveCountMax = 3;
-            hashKnownHosts = false;
-            userKnownHostsFile = "~/.ssh/known_hosts";
-            controlMaster = "no";
-            controlPath = "~/.ssh/master-%r@%n:%p";
-            controlPersist = "no";
-          };
+            };
         };
+      };
     };
-  };
 }
