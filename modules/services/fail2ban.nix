4d9be1ebb448adb6c23edebafc3919bd9ba69916
Michael Strobel <mstrobel97@gmail.com>
2026-03-27 12:33:12 +0100

    fix(fail2ban): use correct ip range in ignoreIP

--- modules/services/fail2ban.nix
+++ modules/services/fail2ban.nix
@@ -6,7 +6,10 @@
   flake.nixosModules.fail2ban = {
     services.fail2ban = {
       enable = true;
-      ignoreIP = [ secrets.pi.ip ];
+      ignoreIP = [
+        secrets.pi.ip
+        "10.0.0.0/24"
+      ];
       bantime = "1h";
     };
   };
