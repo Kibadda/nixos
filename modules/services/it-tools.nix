0f17bd5fc63bf41c3db84c7a645f462cdaf8ab94
Michael Strobel <mstrobel97@gmail.com>
2026-07-16 10:53:01 +0200

    feat(it-tools): open

--- modules/services/it-tools.nix
+++ modules/services/it-tools.nix
@@ -8,6 +8,7 @@
       kibadda.services.it-tools = {
         description = "IT Tools";
         subdomain = "tools";
+        open = true;
         auth = "none";
         extra = ''
           root * ${pkgs.it-tools}/lib
