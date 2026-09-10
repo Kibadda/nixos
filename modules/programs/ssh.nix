459561c2b14d5ca61432c746f8071ac81b514e7f
Michael Strobel <mstrobel97@gmail.com>
2026-09-10 13:52:49 +0200

    chore(ssh): remove commented code

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -96,45 +96,6 @@
                 ControlPersist = "no";
               };
             };
-          # matchBlocks =
-          #   let
-          #     blocks = builtins.listToAttrs (
-          #       map (config: {
-          #         name = config.name;
-          #         value = {
-          #           hostname = config.host;
-          #           port = config.port;
-          #           user = config.user;
-          #           identityFile = lib.mkIf (config.identity != null) "~/.ssh/${config.identity}";
-          #           remoteForwards = lib.mkIf config.forward [
-          #             {
-          #               bind.address = "/run/user/1000/gnupg/S.gpg-agent.ssh";
-          #               host.address = "/run/user/1000/gnupg/S.gpg-agent.ssh";
-          #             }
-          #             {
-          #               bind.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
-          #               host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
-          #             }
-          #           ];
-          #         };
-          #       }) config.kibadda.ssh.hosts
-          #     );
-          #   in
-          #   blocks
-          #   // {
-          #     "*" = {
-          #       forwardAgent = false;
-          #       addKeysToAgent = "no";
-          #       compression = false;
-          #       serverAliveInterval = 0;
-          #       serverAliveCountMax = 3;
-          #       hashKnownHosts = false;
-          #       userKnownHostsFile = "~/.ssh/known_hosts";
-          #       controlMaster = "no";
-          #       controlPath = "~/.ssh/master-%r@%n:%p";
-          #       controlPersist = "no";
-          #     };
-          #   };
         };
       };
     };
